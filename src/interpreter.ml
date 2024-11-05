771b21de6f73bc7073b0596c0c381336a3a831d6
dlustre <dennis.lustre@gmail.com>
2024-11-05 02:03:51 -0800

    evaluate unary operators

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -1,6 +1,10 @@
 open Expr
 
-exception RuntimeError
+exception RuntimeError of Token.t * string
+exception Todo
+
+let is_truthy = function NumOrString _ -> true | Bool b -> b | Nil -> false
+let is_equal = function Nil, Nil -> true | Nil, _ -> false | a, b -> a = b
 
 let interpreter =
   object (self)
@@ -8,5 +12,13 @@ let interpreter =
       function
       | Literal l -> l
       | Grouping expr -> self#evaluate expr
-      | _ -> raise RuntimeError
+      | Unary { operator = Token { kind = MINUS; _ } as negation; right } -> (
+          match self#evaluate right with
+          | NumOrString (Token.NumberLiteral num) ->
+              NumOrString (Token.NumberLiteral ~-.num)
+          | _ -> raise (RuntimeError (negation, "Operand must be a number.")))
+      | Unary { operator = Token { kind = BANG; _ }; right } ->
+          let negated = right |> self#evaluate |> is_truthy |> not in
+          Bool negated
+      | _ -> raise Todo
   end
