3a007b7c08f8eaf7a64799d4105fc2bf575529ee
dlustre <dennis.lustre@gmail.com>
2024-11-05 02:38:17 -0800

    evaluate add and sub

--- src/interpreter.ml
+++ src/interpreter.ml
@@ -6,6 +6,11 @@ exception Todo
 let is_truthy = function Num _ | String _ -> true | Bool b -> b | Nil -> false
 let is_equal = function Nil, Nil -> true | Nil, _ -> false | a, b -> a = b
 
+let binary = function
+  | _, fn, Num left, Num right -> fn left right
+  | operator, _, _, _ ->
+      raise (RuntimeError (operator, "Operands must be numbers."))
+
 let interpreter =
   object (self)
     method evaluate =
@@ -20,14 +25,20 @@ let interpreter =
           let negated = right |> self#evaluate |> is_truthy |> not in
           Bool negated
       | Binary { left; operator = Token { kind = STAR; _ } as operator; right }
-        -> (
-          match (self#evaluate left, self#evaluate right) with
-          | Num left, Num right -> Num (left *. right)
-          | _ -> raise (RuntimeError (operator, "Operands must be a numbers.")))
+        ->
+          Num
+            (binary (operator, ( *. ), self#evaluate left, self#evaluate right))
       | Binary { left; operator = Token { kind = SLASH; _ } as operator; right }
-        -> (
-          match (self#evaluate left, self#evaluate right) with
-          | Num left, Num right -> Num (left /. right)
-          | _ -> raise (RuntimeError (operator, "Operands must be a numbers.")))
+        ->
+          Num
+            (binary (operator, ( /. ), self#evaluate left, self#evaluate right))
+      | Binary { left; operator = Token { kind = PLUS; _ } as operator; right }
+        ->
+          Num
+            (binary (operator, ( +. ), self#evaluate left, self#evaluate right))
+      | Binary { left; operator = Token { kind = MINUS; _ } as operator; right }
+        ->
+          Num
+            (binary (operator, ( -. ), self#evaluate left, self#evaluate right))
       | _ -> raise Todo
   end
